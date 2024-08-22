f14b6be26508e1e0794c265dbab1c949c65303c9
hesom <sommerhoff.hendrik@gmail.com>
2024-08-22 23:18:37 +0200

    Read rom from command line

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use chippers::{chip8::Chip8, frontend::App};
+use clap::Parser;
+use std::path::PathBuf;
 use winit::event_loop::{ControlFlow, EventLoop};
 
+#[derive(Parser, Debug)]
+struct Cli {
+    rom_path: PathBuf,
+
+    #[arg(short, long, default_value_t = 16)]
+    scale: u32,
+}
+
 fn main() {
+    let args = Cli::parse();
+
     let mut chip8 = Chip8::new();
-    chip8.load_rom("./roms/space_invaders.ch8");
+    chip8.load_rom(args.rom_path);
 
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
-    let mut app = App::new(&mut chip8, 16);
+    let mut app = App::new(&mut chip8, args.scale);
 
     event_loop.run_app(&mut app).unwrap();
 }
